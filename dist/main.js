988f11c3d6cc5bf7e924498d0c20b7262d263c51
aymanqabbal <98834007+aymanqabbal@users.noreply.github.com>
2024-07-12 14:28:54 +0100

    .

--- dist/main.js
+++ dist/main.js
@@ -15519,7 +15519,7 @@ const environment = {
   secretToken: '1ddc0870b1f0c10531dc77775e2df37cb897eaedd5acce729fe0dbcc215c9508',
   production: false,
   hmr: false,
-  apiUrl: 'http://127.0.0.1:8000'
+  apiUrl: 'https://008b-41-141-180-183.ngrok-free.app '
 };
 
 /***/ }),
